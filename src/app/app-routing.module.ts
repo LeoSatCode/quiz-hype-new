cefbef8f5a060b91345fe0ba468f8f1c84b799e2
LeoSatCode <your-email@example.com>
2025-02-17 16:45:08 -0300

    app routing update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [
+const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: '**', redirectTo: '' }
 ];
