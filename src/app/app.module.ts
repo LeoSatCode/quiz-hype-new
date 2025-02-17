e9b245556c02ee80127b41cf27b4a6018e4e28cf
LeoSatCode <your-email@example.com>
2025-02-17 16:01:24 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/quiz/quiz.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import quizz_questions from '../../../data/quizz_questions.json'
+
+@Component({
+  selector: 'app-quiz',
+  standalone: false,
+  templateUrl: './quiz.component.html',
+  styleUrl: './quiz.component.css'
+})
+export class QuizComponent {
+
+}
